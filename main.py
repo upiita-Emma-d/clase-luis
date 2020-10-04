d4b630b1ab608ed1784ec7b30c17b0f4ac75918f
Axel Diaz <axtherocknass@gmail.com>
2020-10-03 20:56:50 -0500

    Creado el main

---
+++ main.py
@@ -0,0 +1,5 @@
+clients = 'pablo,ricardo,'
+if __name__ == "__main__":
+    print('Hola aqui empieza el programa')
+    clients += 'Axel'
+    print(clients)
---
+++ tur.py
@@ -0,0 +1,8 @@
+import turtle
+
+window = turtle.Screen()
+tortuga = turtle.Turtle()
+
+tortuga.forward(100)
+
+window.mainloop()
